6ab5907f79691e3fe341dc9b933ffdbaff441d0b
RoberSerr <robertoserrano@robertoserrano.es>
2023-04-24 10:14:30 +0200

    swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+
 
 class HomeScreen extends StatelessWidget {
 
@@ -11,8 +15,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Placeholder(),
+        child: _HomeView(),
       ),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    return Column(
+      children: [
+
+        const CustomAppbar(),
+
+        MoviesSlideshow(movies: nowPlayingMovies)
+        
+      ],
     );
   }
 }
